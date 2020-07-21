77c570b883f772ad8c6fa72405f17a5ee46eeb44
zziro12 <opers7@naver.com>
2020-07-21 18:28:52 +0900

    Initial Commit

---
+++ MyPicture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPicture
+//
+//  Created by Jinho Choi on 2020/07/21.
+//  Copyright © 2020 Jinho Choi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
